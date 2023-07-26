5944957b19403d51497a7632ba9ed4b9a0908c3e
Shova <mhrzashova12345@gmail.com>
2023-07-26 08:36:33 +0545

    project

--- admin/brandupdate.php
+++ admin/brandupdate.php
@@ -37,80 +37,8 @@ if (isset($_POST['brand_edit'])) {
 
    </head>
 <body>
-  <div class="sidebar">
-    <div class="logo-details">
-      <i class='bx bxl-c-plus-plus'></i>
-      <span class="logo_name">Car Rental Portal</span>
-    </div>
-      <ul class="nav-links">
-        <li>
-          <a href="admindashboard.php" class="active">
-            <i class='bx bx-grid-alt' ></i>
-            <span class="links_name">Dashboard</span>
-          </a>
-        </li>
-        <li>
-          <a href="createbrand.php" class="active">
-            <i class='bx bx-book-alt'></i>
-            <span class="links_name">Brand</span>
-          </a>
-        </li>
-        <li>
-          <a href="create.php">
-            <i class='bx bx-car'></i>
-            <span class="links_name">Vehicles</span>
-          </a>
-        </li>
-        <li>
-          <a href="customerlist.php">
-            <i class='bx bx-list-ul' ></i>
-            <span class="links_name">Registered Customer</span>
-          </a>
-        </li>
-        <li>
-          <a href="#" id="bookingsMenu">
-            <i class='bx bx-book-alt'></i>
-            <span class="links_name">Bookings</span>
-            <i class='bx bxs-chevron-down arrow'></i>
-          </a>
-          <ul class="sub-menu" id="bookingsSubMenu">
-            <li>
-              <a href="#">
-                <span class="links_name">New</span>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <span class="links_name">Confirmed</span>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <span class="links_name">Cancelled</span>
-              </a>
-            </li>
-          </ul>
-        </li>
-        <li class="sub-menu-item" id="analyticsItem">
-          <a href="#">
-            <i class='bx bx-pie-chart-alt-2'></i>
-            <span class="links_name">Analytics</span>
-          </a>
-        </li>
-        <li class="sub-menu-item" id="stockItem">
-          <a href="#">
-            <i class='bx bx-coin-stack'></i>
-            <span class="links_name">Stock</span>
-          </a>
-        </li>
-        <li class="log_out">
-          <a href="index.php">
-            <i class='bx bx-log-out'></i>
-            <span class="links_name">Log out</span>
-          </a>
-        </li>
-      </ul>
-  </div>
+  <?php include('includes/header.php');?>
+  
   <section class="home-section">
     <nav>
       <div class="sidebar-button">
--- admin/read.php
+++ admin/read.php
@@ -10,80 +10,8 @@
 
    </head>
 <body>
-  <div class="sidebar">
-    <div class="logo-details">
-      <i class='bx bxl-c-plus-plus'></i>
-      <span class="logo_name">Car Rental Portal</span>
-    </div>
-      <ul class="nav-links">
-        <li>
-          <a href="admindashboard.php" class="active">
-            <i class='bx bx-grid-alt' ></i>
-            <span class="links_name">Dashboard</span>
-          </a>
-        </li>
-        <li>
-          <a href="createbrand.php" class="active">
-            <i class='bx bx-book-alt'></i>
-            <span class="links_name">Brand</span>
-          </a>
-        </li>
-        <li>
-          <a href="create.php">
-            <i class='bx bx-car'></i>
-            <span class="links_name">Vehicles</span>
-          </a>
-        </li>
-        <li>
-          <a href="customerlist.php">
-            <i class='bx bx-list-ul' ></i>
-            <span class="links_name">Registered Customer</span>
-          </a>
-        </li>
-        <li>
-          <a href="#" id="bookingsMenu">
-            <i class='bx bx-book-alt'></i>
-            <span class="links_name">Bookings</span>
-            <i class='bx bxs-chevron-down arrow'></i>
-          </a>
-          <ul class="sub-menu" id="bookingsSubMenu">
-            <li>
-              <a href="#">
-                <span class="links_name">New</span>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <span class="links_name">Confirmed</span>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <span class="links_name">Cancelled</span>
-              </a>
-            </li>
-          </ul>
-        </li>
-        <li class="sub-menu-item" id="analyticsItem">
-          <a href="#">
-            <i class='bx bx-pie-chart-alt-2'></i>
-            <span class="links_name">Analytics</span>
-          </a>
-        </li>
-        <li class="sub-menu-item" id="stockItem">
-          <a href="#">
-            <i class='bx bx-coin-stack'></i>
-            <span class="links_name">Stock</span>
-          </a>
-        </li>
-        <li class="log_out">
-          <a href="index.php">
-            <i class='bx bx-log-out'></i>
-            <span class="links_name">Log out</span>
-          </a>
-        </li>
-      </ul>
-  </div>
+  <?php include('includes/header.php');?>
+  
   <section class="home-section">
     <nav>
       <div class="sidebar-button">
@@ -161,41 +89,8 @@
     </div>
   </section>
 
-  <script>
-    let sidebar = document.querySelector(".sidebar");
-    let sidebarBtn = document.querySelector(".sidebarBtn");
-    sidebarBtn.onclick = function() {
-      sidebar.classList.toggle("active");
-      if(sidebar.classList.contains("active")){
-      sidebarBtn.classList.replace("bx-menu" ,"bx-menu-alt-right");
-    }else
-      sidebarBtn.classList.replace("bx-menu-alt-right", "bx-menu");
-    }
-    // Get the "Analytics" and "Stock" menu elements
-    const analyticsItem = document.getElementById("analyticsItem");
-    const stockItem = document.getElementById("stockItem");
-    const bookingsSubMenu = document.getElementById("bookingsSubMenu");
-
-    // Function to toggle the visibility of "Analytics" and "Stock" items
-    function toggleSubItems() {
-      bookingsSubMenu.classList.toggle("active");
-
-      // Calculate the height of the sub-menu
-      const subMenuHeight = bookingsSubMenu.offsetHeight;
-
-      // Adjust the position of the "Analytics" and "Stock" items
-      if (bookingsSubMenu.classList.contains("active")) {
-        analyticsItem.style.transform = `translateY(${subMenuHeight}px)`;
-        stockItem.style.transform = `translateY(${subMenuHeight}px)`;
-      } else {
-        analyticsItem.style.transform = "translateY(0)";
-        stockItem.style.transform = "translateY(0)";
-      }
-    }
-
-    // Attach a click event listener to the "Bookings" menu
-    bookingsMenu.addEventListener("click", toggleSubItems);
-  </script>
+  <!-- Link To JS -->
+<script src="js/all.js"></script>
 
 </body>
 </html>
