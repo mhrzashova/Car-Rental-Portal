12518d073f34f6b23eb5734e17e7945312d282dc
mhrzashova <maharjan.shovaa@gmail.com>
2024-11-16 18:37:41 +0545

    new

--- rent.php
+++ rent.php
@@ -13,7 +13,7 @@ $user_id = $_SESSION['users']['user_id'];
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="UTF-8">    
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Car Rental Portal</title>
